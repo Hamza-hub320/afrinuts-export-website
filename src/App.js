d5a66263365b3aaa5f61227791952300da731dc5
Hamza Sa'eed Ralsan Sangare <saeedralsans@gmail.com>
2025-05-30 10:16:06 +0100

    Fix nested routing with proper layout structure

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <BrowserRouter basename="/afrinuts-export-website">
-      <Layout>
-        <Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/farm" element={<Farm />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Layout>
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="farm" element={<Farm />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
